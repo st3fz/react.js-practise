5e031388564c1aad2fc7e0e9347624b8e15ee000
Steffi <steffi__98@hotmail.com>
2021-09-13 18:38:26 +0800

    📛 Renamed Id and Username components

--- src/components/cards/Id.tsx
+++ src/components/cards/Id.tsx
@@ -32,7 +32,7 @@ const Id : React.FC<Props> = ( props : Props ) => {
             <ReactTooltip id="counter-info">
                 <p>A random ID is generated each time you click refresh</p>
                 <p className="text-bold">What is ID?</p>
-                <p className="text-bold">Your ID is used to generate your username</p>
+                <p className="text-bold">Your ID is added to the back of your username, to always ensure that you have a unique username</p>
             </ReactTooltip> 
             <h2>Your ID: {id}
                 <span data-tip data-for="counter-info">
@@ -41,7 +41,6 @@ const Id : React.FC<Props> = ( props : Props ) => {
             </h2>
             <button className="btn rounded-circle btn-dark mr-3" onClick={()=>modifyId("+")}>+</button>
             <button className="btn rounded-circle btn-dark" onClick={()=>modifyId("-")}>-</button>
-            <br/>
             <button className="btn" onClick={()=>generateNewId()}><FaRedoAlt/></button>
         </div>
     )
