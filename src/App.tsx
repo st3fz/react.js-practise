331ba05312b222386e77a4f2e901eaf6a1941051
Steffi <steffi__98@hotmail.com>
2021-09-13 18:42:40 +0800

    🌀 Refactored some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,65 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Welcome from './components/cards/Welcome';
 import Id from './components/cards/Id';
-import Form from './components/cards/Form';
-import Callback from './components/cards/Callback';
-import ImageSlider from './components/cards/ImageSlider';
+import Authentication from './components/cards/Authentication';
+import Username from './components/cards/Username';
+import DisplayPicture from './components/cards/DisplayPicture';
+
+import ReactDOM from 'react-dom';
 
 const App :  React.FC = () => {
 
   const [ fullName , setFullName ] = useState<string>("");
   const [ randomId , setRandomId ] = useState<number>(NaN);
 
+  const getAllCardElements = () => {
+    var elements = [];
+    for (let x = 1; x <= 12; x++) {
+      var noOfElements = document.getElementsByClassName("col-"+x).length;
+      for (let i = 0; i < noOfElements-1; i++) {
+        var element = document.getElementsByClassName("col-"+x)[i]; 
+        elements.push(element);
+      }
+    }
+    console.log(elements);
+  } 
+
+  useEffect(() => {
+    getAllCardElements();
+  })
+
   return (
-    <div className="container text-center">
+    <div className="container text-center align-self-center">
       
       {/* Components */}
-      <div className="row align-items-center">
-        <div className="col-6">
+      <div className="row">
+        <div className="col-9">
           <Welcome/> 
         </div>
         <div className="col-3">
           <Id setRandomId={setRandomId}/>
         </div>
-        <div className="col-3">
-          <ImageSlider/>
+        <div className="col-6">
+          <DisplayPicture/>
         </div>
-        <div className="col-9">
-          <Form/>
+        <div className="col-6">
+          <Username setFullName={setFullName}/>
         </div>
-        <div className="col-9">
-          <Callback setFullName={setFullName}/>
+        <div className="col-6">
+          <Authentication/>
         </div>
       </div>
       
       {/* // Filled in details */}
-      {/* <div>
-        Name: {fullName}
-        Random Id: {randomId}
-      </div> */}
-      
+      <div>
+        <button className="btn btn-warning">SIGN UP</button> <br/>
+        //To be displayed in model after clicking sign up <br/>
+        Username: {fullName}#{randomId} <br/>
+        Random Id:  <br/>
+      </div>
     </div>
   );
-  
 
 }
 
